b83b8343c9fd980cc6126e7028f089b3cda9ff0e
Pedro Eduardo <eduardototi@gmail.com>
2024-11-10 09:06:59 -0300

    Add translate

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,5 +3,5 @@ class Rating < ApplicationRecord
   belongs_to :post
 
   validates :value, presence: true, inclusion: { in: 1..5 }
-  validates :user_id, uniqueness: { scope: :post_id, message: "has already rated this post." }
+  validates :user_id, uniqueness: { scope: :post_id, message: I18n.t("already_rated") }
 end
